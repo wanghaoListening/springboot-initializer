938530dabd192cf94a525d73a34c267ac1bdf97e
王浩 <haowang162@creditease.cn>
2019-06-17 20:37:58 +0800

    添加数据库配置

---
+++ src/main/java/com/haothink/config/MyBatisConfig.java
@@ -0,0 +1,16 @@
+package com.haothink.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author wanghao
+ * @date 2019年06月17日 19:49
+ * description:
+ */
+@Configuration
+@MapperScan("com.haothink.dao")
+public class MyBatisConfig {
+
+
+}
