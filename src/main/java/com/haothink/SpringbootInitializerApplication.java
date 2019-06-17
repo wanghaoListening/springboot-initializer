03cce3904a178cb82eb109293db86928f89dd6c1
王浩 <haowang162@creditease.cn>
2019-06-17 19:01:54 +0800

    springboot init

---
+++ src/main/java/com/haothink/SpringbootInitializerApplication.java
@@ -0,0 +1,13 @@
+package com.haothink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInitializerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootInitializerApplication.class, args);
+    }
+
+}
