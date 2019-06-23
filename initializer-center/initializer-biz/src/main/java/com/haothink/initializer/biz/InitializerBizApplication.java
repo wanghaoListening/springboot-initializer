fa4edfac19ca60d06f2af4511829f95650c3d20b
王浩 <haowang162@creditease.cn>
2019-06-23 17:04:00 +0800

    添加mybatis

--- initializer-center/initializer-biz/src/main/java/com/haothink/initializer/biz/InitializerBizApplication.java
+++ initializer-center/initializer-biz/src/main/java/com/haothink/initializer/biz/InitializerBizApplication.java
@@ -1,9 +1,11 @@
 package com.haothink.initializer.biz;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.haothink.initializer.biz.dao")
 public class InitializerBizApplication {
 
     public static void main(String[] args) {
