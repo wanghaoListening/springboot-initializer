d9d94de7ec441daf7fbfe0ed8d25db737df0ff2a
wanghaoListening <wanghaotime@qq.com>
2019-07-20 16:52:09 +0800

    添加定时任务

--- initializer-center/initializer-biz/src/main/java/com/haothink/initializer/biz/InitializerBizApplication.java
+++ initializer-center/initializer-biz/src/main/java/com/haothink/initializer/biz/InitializerBizApplication.java
@@ -3,7 +3,10 @@ package com.haothink.initializer.biz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+//开启定时任务,在需要跑任务的方法上加@Scheduled(cron="*/6 * * * * ?")
+@EnableScheduling
 @SpringBootApplication
 @MapperScan("com.haothink.initializer.biz.dao")
 public class InitializerBizApplication {
